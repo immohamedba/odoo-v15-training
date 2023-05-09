a28decd4ea9d165e0e485d9be7097e5ff60a603d
medBa-fnd <mohamed.abdoulah.ba@gmail.com>
2023-05-09 15:00:50 +0100

    51

--- project_assignment/models/projectAssignment.py
+++ project_assignment/models/projectAssignment.py
@@ -8,4 +8,15 @@ class ProjectAssignment(models.Model):
     _rec_name = 'project_name'
 
     project_name = fields.Many2one(comodel_name='project.project', string='project Name', required=True)
-    employees_list_ids = fields.One2many('employees.list', 'project_id', string='Employees')
+    weighting = fields.Selection([
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+        ('7', '7'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10')], default='1', string="weighting")
+    employees_list_ids = fields.Many2many('hr.employee',  string='Employees')
