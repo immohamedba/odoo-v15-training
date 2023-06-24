ae2abae01fa73d46ee3592f421c4ded92fb658ad
mohamed fares <faresabidi21@gmail.com>
2023-06-24 16:18:58 +0100

    60

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Hospital Management",
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': "Med Fares",
+    'sequence': -450,
+    'summary': 'Hospital management system',
+    'description': """ Hospital management system """,
+    'depends': ['mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/female_patient_view.xml',
+        'views/male_patient_view.xml',
+        'views/appointment_view.xml',
+        'views/patient_tag_view.xml',
+    ],
+    'demo': [],
+
+    'auto-install': False,
+    'installable': True,
+    'license': 'LGPL-3',
+    'application': True,
+}
